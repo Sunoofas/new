cc6fdf220df33a149012e030e4f43ebd6f2d202a
unknown <Dell@CDCINDTVMLAP12.cdcllp.local>
2023-05-05 18:31:28 +0530

    first commit

--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -3,26 +3,24 @@ const db = require("../database/todoDb");
 //To add data to db
 const putTask = async (req, res) => {
   console.log('insideputTask :>> ');
-  console.log("req.body", req.body, "req", req);
+  // console.log("req.body", req.body, "req", req);
+  console.log('inside putTask');
   try {
-    const result = await db.any(
-      "INSERT INTO todo(data) VALUES ($1) RETURNING",
-      [req.body]
-    );
-    console.log("resultput", result);
-    // res.send(result);
-    res.end();
+    const query = 'INSERT INTO todos(task) VALUES ($1)';
+    const values = [req.body.task];
+    const result = await db.query(query, values);
+    console.log('Data inserted successfully');
+    res.send(result);
   } catch (err) {
     console.log(err);
-    res.send("Error" + err);
-    res.send();
+    res.send('Error' + err);
   }
 };
 
 const getTask = async (req, res) => {
   console.log('Inside get todo :>> ');
   try {
-    const result = await db.any("SELECT * FROM todo");
+    const result = await db.query("SELECT * FROM todos");
 
     res.send(result);
   } catch(err){
@@ -31,7 +29,22 @@ const getTask = async (req, res) => {
   }
 };
 
+const deleteTask = async(req, res)=>{
+  console.log('deletedvalue :>> ');
+  try{
+    const result = await db.query(" DELETE FROM todos (req.body) VALUES ", [req.body]);
+ 
+  console.log('removedValue :>> ', result);
+  res.send(result);
+  }
+  catch(err){
+    console.log(err);
+  }
+
+}
+
 module.exports = {
   putTask,
   getTask,
+  deleteTask
 };
